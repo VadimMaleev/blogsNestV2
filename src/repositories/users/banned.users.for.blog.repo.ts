f3543d2bc8b11d1fa116835804e4af68732a9ffa
VadimMaleev <maleeffset@yandex.by>
2023-06-20 15:22:36 +0300

    sort fix

--- src/repositories/users/banned.users.for.blog.repo.ts
+++ src/repositories/users/banned.users.for.blog.repo.ts
@@ -44,9 +44,9 @@ export class BannedUsersForBlogRepository {
         blogId: blogId,
         login: { $regex: loginSearchTerm, $options: 'i' },
       })
+      .sort({ [sortBy]: sortDirection })
       .skip((pageNumber - 1) * pageSize)
-      .limit(pageSize)
-      .sort({ [sortBy]: sortDirection });
+      .limit(pageSize);
 
     const itemsForResponse = items.map(mapBannedUsersForBlog);
     const totalCount = await this.bannedUserForBlogModel.count({
